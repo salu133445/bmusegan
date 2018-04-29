cef7a36b2cfb89aef855cbba409e99de186024fb
salu133445 <salu133445@gmail.com>
2018-04-29 23:26:28 +0800

    Fix architecture bugs

--- musegan/bmusegan/presets/bar/refiner/proposed_round.py
+++ musegan/bmusegan/presets/bar/refiner/proposed_round.py
@@ -5,12 +5,12 @@ NET_R = {}
 NET_R['private'] = [
     ('identity', None, None, None),
     ('identity', None, 'bn', 'relu'),
-    ('conv3d', (64, (3, 12), (1, 1), 'SAME'), 'bn', 'relu'),
-    ('conv3d', (1, (3, 12), (1, 1), 'SAME'), None, None),
+    ('conv2d', (64, (3, 12), (1, 1), 'SAME'), 'bn', 'relu'),
+    ('conv2d', (1, (3, 12), (1, 1), 'SAME'), None, None),
     ('identity', None, None, None, ('add', 0)),
     ('identity', None, 'bn', 'relu'),
-    ('conv3d', (64, (3, 12), (1, 1), 'SAME'), 'bn', 'relu'),
-    ('conv3d', (1, (3, 12), (1, 1), 'SAME'), None, None),
+    ('conv2d', (64, (3, 12), (1, 1), 'SAME'), 'bn', 'relu'),
+    ('conv2d', (1, (3, 12), (1, 1), 'SAME'), None, None),
     ('identity', None, None, 'round', ('add', 4)),
-    ('reshape', (1, 96, 84, 1))
+    ('reshape', (1, 96, 84, 1)),
 ]
